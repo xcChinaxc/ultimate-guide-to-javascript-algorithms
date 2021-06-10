78ceac8a32783bab5e3349861836bf1bf0b8abd0
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-10 12:17:44 -0400

    calculate and return the the hamming distance

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -3,13 +3,31 @@ Given two strings of equal length, calculate and return the the hamming distance
 E.g hammingDistance('rover', 'river') // should return 1
 */
 
-
-
-
+// First Try
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    let num = []
+    if(stringA.length === stringB.length){
+        for(let i=0; i < stringA.length; i++){
+            num.push(stringA[i] === stringB[i])
+        }
+    }
+    return num.filter(el => el == false).length
 }
 
+// For Loop
+function hammingDistance(stringA, stringB){
+    let result = 0
+    if( stringA.length == stringB.length ){
+        for( let i = 0; i < stringA.length; i++ ){
+            if( stringA[i].toLowerCase() != stringB[i].toLowerCase() ){
+                result++
+            }
+        }
+        return result
+    } else {
+        throw new Error('Strings do not have equal length')
+    }
+}
 
 
 module.exports = hammingDistance
