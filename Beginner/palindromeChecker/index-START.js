6240bd2d6c290e63b28f7b38a8d28480b8c35df8
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-10 11:43:52 -0400

    return whether the text is a palindrome or not

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,36 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
+// First Try
 function palindromeChecker(text) {
-    v// Code goes here
+    reversed = text.split("").reverse().join("")
+    return text === reversed
 }
 
+// An Intuitive Approach
+function palindromeChecker(text) {
+    reversed = text.toLowerCase().split("").reverse().join("")
+    return text === reversed
+}
 
+// Looping Through and Comparing Characters
+function palindromeChecker(text){
+    let charArry = text.toLowerCase().split(""),
+    result = charArry.every( (letter, index) => {
+        return letter === charArry[charArry.length - index - 1]
+    } )
+    return result
+}
+
+// Looping Through and Comparing Characters(Optimized)
+function palindromeChecker(text){
+    let textLen = text.length
+    for(let i=0; i < textLen / 2; i++){
+        if(text[i] !== text[textLen - 1 -i]){
+            return false
+        }
+    }
+    return true
+}
 
 module.exports = palindromeChecker;
