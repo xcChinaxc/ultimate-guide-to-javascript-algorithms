7659d08c4c809eba4cd5796c7f5a8b17b2f36f51
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-10 13:16:31 -0400

    added given solutions

---
+++ Beginner/searchReplace/index-SOLUTION.js
@@ -0,0 +1,16 @@
+// USING FOR REPLACE
+function searchReplace(str, word, newWord){
+    if( word[0] === word[0].toUpperCase() ){
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(word, newWord)
+}
+
+// USING FOR REGULAR EXPRESSIONS
+function searchReplace(str, word, newWord){
+    let regex = new RegExp(word, "gi")
+    if( /[A-Z]/.test(word[0]) ){
+        newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(regex, newWord)
+}
