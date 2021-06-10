24bdec40d6a3c1fcf3f2a392c75ebd384bf03c18
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-10 10:48:39 -0400

    returns the text received in a reversed format

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,32 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
+// First Try
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("")
 }
 
+// For Loop
+function reverseString(text) {
+    let result = ""
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
+    return result
+}
 
+// Recursion Method
+function reverseString(text){
+    let result = ""
+    for (let char of text){
+        result = char + result
+    }
+    return result
+}
+
+// .reduce()
+function reverseString(text){
+    return text.split("").reduce( (acc, char) => char + acc, '')
+}
 
 module.exports = reverseString
