b8b4083686834e8fd01491ab4ec0526fdeb8493d
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-11 14:55:22 -0400

    followed along. combine arrays with no duplicates

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -1,21 +1,41 @@
-/*
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
+/* CHALLENGE
+ Given two or more arrays, write a function that combines their elements into one array without any repetition. 
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function mergeArrays(...arrays) {
-
+// Using Sets
+function mergeArrays(...arrays){
     let jointArray = []
-    
     arrays.forEach(array => {
         jointArray = [...jointArray, ...array]
-    });
-
-    return [...new Set([...jointArray])]
-
-    
+    })
+    return Array.from(new Set([...jointArray]))
 }
 
+// Using the filter method
+function mergeArrays(...arrays){
+    let jointArray = []
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    })
+    const uniqueArr = jointArray.filter( (e,i) => jointArray.indexOf(e) === i)
+    return uniqueArr
+}
+
+// Using the reduce method
+function mergeArrays(...arrays){
+    let jointArray = []
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    })
+    const uniqueArr = jointArray.reduce( (newArr, item) => {
+        if(newArr.includes(item)){
+            return newArr
+        } else {
+            return [...newArr, item]
+        }
+    }, [])
+    return uniqueArr
+}
 
 module.exports = mergeArrays
