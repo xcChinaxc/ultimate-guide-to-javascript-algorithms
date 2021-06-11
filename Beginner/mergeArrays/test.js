fb92a1f2212384aea9da12301083a0faf025c2eb
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-11 14:22:58 -0400

    fixed typo

--- Beginner/mergeArrays/test.js
+++ Beginner/mergeArrays/test.js
@@ -4,11 +4,11 @@ test('mergeArrays is a function', () => {
   expect(typeof mergeArrays).toEqual('function');
 });
 
-test('Combines 5 arrays of numbers without dubplicates', () => {
+test('Combines 5 arrays of numbers without duplicates', () => {
   expect(mergeArrays([1,2],[2,3],[3,4],[4,5])).toEqual([1,2,3,4,5]);
 });
 
-test('Combines 3 arrays of strings without dubplicates', () => {
+test('Combines 3 arrays of strings without duplicates', () => {
   expect(mergeArrays(['a','b','z'],['m','n','a'],['z','y'])).toEqual(['a','b', 'z', 'm', 'n', 'y']);
 });
 
