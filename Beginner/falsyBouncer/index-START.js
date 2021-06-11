3af094021325fc589902d7b71be6940f73ccd568
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-11 15:10:14 -0400

    remove falsy values and return only truthy values

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -1,14 +1,30 @@
-/*
-Given an array, remove all falsy values from the array
-and return an array of only truthy values.
+/* CHALLENGE
+Given an array, remove all falsy values from the array and return an array of only truthy values.
 
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// First Try
 function falsyBouncer(array) {
-    // Code goes here
+    return array.filter( (e) => Boolean(e))
 }
 
+// Using a for...of loop
+function falsyBouncer(array){
+    let result = []
+    for(value of array){
+        if(value){
+            result.push(value)
+        }
+    }
+    return result
+}
+
+// Using filter method
+function falsyBouncer(array) {
+    return array.filter( (value) => {
+        return Boolean(value)
+    })
+}
 
 module.exports = falsyBouncer
